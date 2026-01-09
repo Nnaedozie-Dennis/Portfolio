bb9b79bd2d44a3c7a238029abfd069611c82c6bc
Nnaedozie Dennis <dennisnnaedozie@gmail.com>
2026-01-09 07:43:53 +0100

    Fix

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,9 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-//   experimental: {
-//     turbo: false, // ⛔ disable Turbopack (fixes the font error)
-// },
-// };
-
-// export default nextConfig;
 
 
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    turbo: false, // ⛔ disable Turbopack (fixes the font error)
-  },
+  experimental: {},
 };
 
 export default nextConfig;
